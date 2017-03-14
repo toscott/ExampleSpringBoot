b4d814dcf5d9f0c6d6636f4f0a6bdee4f2e00143
jkurtz <Niscpass5>
2017-03-14 15:34:12 -0500

    it works!

---
+++ example/src/main/java/com/example/helloworld/User.java
@@ -0,0 +1,38 @@
+package com.example.helloworld;
+
+/**
+ * Created by jkurtz on 3/14/2017.
+ */
+public class User
+{
+    private String name;
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    private Integer id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
